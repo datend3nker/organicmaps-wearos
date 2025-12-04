8b824fc65f2a307d3e7dc6921ad56319e809014b
Ludwig Ettner <ludwig.ettner@web.de>
2025-12-04 11:02:15 +0100

    wearos app initialised

---
+++ android/app/omaps/src/main/java/com/example/omaps/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.omaps.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun OrganicMapsTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
